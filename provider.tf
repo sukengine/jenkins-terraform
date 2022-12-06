72899e24b6bdd143c58d163fc55cfdb9acb16a79
sukengine <51234432+sukengine@users.noreply.github.com>
2022-12-06 10:53:43 +0700

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,20 +1,4 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "4.18.0"
-    }
-    local = {
-      source  = "hashicorp/local"
-      version = "2.2.3"
-    }
-    tls = {
-      source  = "hashicorp/tls"
-      version = "3.4.0"
-    }
-  }
-  required_version = ">= 1.0.0"
-}
+
 
 provider "aws" {
   access_key = var.aws_access_key
